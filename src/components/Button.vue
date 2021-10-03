68fad75441d2b9281f73f3ef3749286fc18fef49
Jason Chicas <rabelyj@gmail.com>
2021-10-03 20:35:46 +0200

    upload project

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="$emit('toggle-add-task')"  :style="{ background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script>
+
+export default {
+  name: 'Button',
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick() {
+      console.log('Adding new tasks');
+    }
+  },
+}
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Version 1.0.0</h3>
+  <router-link to="/">Go back</router-link>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  props: {
+    showAddTask: Boolean,
+  }
+}
+</script>
